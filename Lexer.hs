19b1ea0b4e1d2470d1425c123cf7e7a17b36643f
simonski <s.hehnen@campus.lmu.de>
2021-06-06 17:30:07 +0200

    added data Operator to Lexer.hs

--- Lexer.hs
+++ Lexer.hs
@@ -2,10 +2,35 @@ module Lexer (Token(..), tokenize) where
 
 import Data.Char ( isAlpha, isDigit, isSpace )
 
+data Operator = Plus | Minus | Times | Div | And | Or | LessThen | BranketA | BranketB
+    | Semicolon | If | Then | Else | Not | True | False | Equal | Assignment
+    deriving (Show, Eq)
 
-data Token = NumberToken Integer | NameToken String | KeywordToken String deriving Eq
+data Token = NumberToken Integer | NameToken Operator | KeywordToken Operator deriving (Show, Eq)
 
+operator :: String -> Operator
+operator c | c == "+"       = Plus
+           | c == "-"       = Minus
+           | c == "*"       = Times
+           | c == "/"       = Div
+           | c == "("       = BranketA
+           | c == ")"       = BranketB
+           | c == ";"       = Semicolon
+           | c == "<"       = LessThen
+           | c == "&"       = And
+           | c == "|"       = Or
+           | c == "="       = Assignment
+           | c == "=="      = Equal
+           | c == "if"      = Lexer.If
+           | c == "then"    = Then
+           | c == "else"    = Else
+           | c == "not"     = Not
+           | c == "true"    = Lexer.True
+           | c == "false"   = Lexer.False
+
+symbols :: [Char]
 symbols = ['(', ')', '&', '|', '<', '+', '-', '*', '/', ';']
+keywords :: [String]
 keywords = ["if", "then", "else", "not", "true", "false"]
 
 tokenize :: String -> [Token]
@@ -15,10 +40,10 @@ tokenize (c:s)
     | isAlpha c = let token = c : takeWhile isAlpha s 
                       rest =  dropWhile isAlpha s 
                   in if token `elem` keywords
-                      then  KeywordToken token : tokenize rest
-                      else NameToken token : tokenize rest
+                      then  KeywordToken (operator token) : tokenize rest
+                      else NameToken (operator token) : tokenize rest
     | isDigit c = NumberToken (read(c : takeWhile isDigit s)) : tokenize (dropWhile isDigit s)
-    | c `elem` symbols = KeywordToken [c] : tokenize s
+    | c `elem` symbols = KeywordToken (operator [c]) : tokenize s
     | c == '=' = case s of
-        '=':s' -> KeywordToken "==" : tokenize s'
-        _ -> KeywordToken "=" : tokenize s
+        '=':s' -> KeywordToken Equal : tokenize s'
+        _ -> KeywordToken Assignment : tokenize s
