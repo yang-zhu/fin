2f46f95ee00ce822c73aa98717156016b36ed460
Zhu Yang <zhuyang329@gmail.com>
2021-07-23 18:16:37 +0200

    cleaned up code in FCompiler.hs added comments formatted the code made some variable names succinct

--- draft/FCompiler.hs
+++ draft/FCompiler.hs
@@ -1,56 +1,132 @@
 module FCompiler (translateProgram) where
 
-import Parser
-    ( Variable,
-      Program,
-      AtomicExpression(Var, Number, TruthValue),
-      Expression(..),
-      LocalDefinition(..),
-      Definition(..) )
-import MF
-    ( MachineState(..),
-      HeapCell(DEF),
-      Operator(IfOperator, UnaryOperator, BinaryOperator),
-      Instruction(..),
-      Value(BoolValue, IntValue) )
 import qualified Data.Map.Strict as Map
+import Data.Sequence ((|>))
 import qualified Data.Sequence as Sequence
-import Data.Sequence ( (|>) )
+import MF
+  ( HeapCell (DEF),
+    Instruction (..),
+    MachineState (..),
+    Operator (BinaryOperator, IfOperator, UnaryOperator),
+    Value (BoolValue, IntValue),
+  )
+import Parser
+  ( AtomicExpression (Number, TruthValue, Var),
+    Definition (..),
+    Expression (..),
+    LocalDefinition (..),
+    Program,
+    Variable,
+  )
 
-
+-- Translate function definitions
 translateDef :: Definition -> [Instruction]
-translateDef (Definition f args body) = translateKons body pos ++ [UpdateFun (length args), Slide (length args + 1), Unwind, Call, Return] where
-    pos = zip args [1..]
+translateDef (Definition f args body) =
+  translateExpr body pos
+    ++ [UpdateFun (length args), Slide (length args + 1), Unwind, Call, Return]
+  where
+    -- pos is a list of pairs (variable, variable's position in the stack)
+    pos = zip args [1 ..]
 
+-- Translate local definitions
+translateLocalDefs :: [LocalDefinition] -> [(Variable, Int)] -> [Instruction]
+translateLocalDefs [] pos = []
+translateLocalDefs (LocalDef v e : lDefs) pos =
+  translateExpr e pos
+    ++ [UpdateLet (length lDefs)]
+    ++ translateLocalDefs lDefs pos
 
-translateLocalDefinitions :: [LocalDefinition] -> [(Variable, Int)] -> [Instruction]
-translateLocalDefinitions [] pos = []
-translateLocalDefinitions (LocalDef v e : lDefs) pos = translateKons e pos ++ [UpdateLet (length lDefs)] ++ translateLocalDefinitions lDefs pos
-
-translateKons :: Expression -> [(Variable, Int)] -> [Instruction]
-translateKons (Let localDefs expr) pos = concat (replicate (length localDefs) [Alloc, Alloc, Makeapp]) ++ translateLocalDefinitions localDefs pos' ++ translateKons expr pos' ++ [SlideLet (length localDefs)] where
-    variables = [ v | LocalDef v _ <- localDefs]
-    varPos = zip (reverse variables) [-1..]
+-- Translate expressions
+translateExpr :: Expression -> [(Variable, Int)] -> [Instruction]
+translateExpr (Let localDefs e) pos =
+  -- first reserve space in the heap and stack for all the bindings
+  -- all the local definitions are accessible from the beginning
+  concat (replicate (length localDefs) [Alloc, Alloc, Makeapp])
+    ++ translateLocalDefs localDefs pos'
+    ++ translateExpr e pos'
+    ++ [SlideLet (length localDefs)]
+  where
+    variables = [v | LocalDef v _ <- localDefs]
+    varPos = zip (reverse variables) [-1 ..]
     numVars = length variables
-    pos' = varPos ++ map (\(x,y) -> (x, y + numVars)) pos
+    pos' = varPos ++ map (\(x, y) -> (x, y + numVars)) pos
     -- recursive let in Haskell does not allow pos = varPos ++ map (\(x,y) -> (x, y + numVars)) pos, because the pos in the body refers to the newly defined pos
-translateKons (FuncApp e1 e2) pos = translateKons e2 pos ++ translateKons e1 (map (\(x,y) -> (x, y+1)) pos) ++ [Makeapp]
-translateKons (Unary op e) pos = translateKons e pos ++ [Pushpre (UnaryOperator op), Makeapp]
-translateKons (Binary e1 op e2) pos = translateKons e2 pos ++ translateKons e1 (map (\(x,y) -> (x, y+1)) pos) ++ [Pushpre (BinaryOperator op), Makeapp, Makeapp]
-translateKons (If e1 e2 e3) pos = translateKons e3 pos ++ translateKons e2 (map (\(x,y) -> (x, y+1)) pos) ++ translateKons e1 (map (\(x,y) -> (x, y+2)) pos) ++ [Pushpre IfOperator, Makeapp, Makeapp, Makeapp]
-translateKons (AtomicExpr (Number i)) pos = [Pushval (IntValue i)]
-translateKons (AtomicExpr (TruthValue b)) pos = [Pushval (BoolValue b)]
-translateKons (AtomicExpr (Var v)) pos = case lookup v pos of
+translateExpr (FuncApp e1 e2) pos =
+  translateExpr e2 pos
+    ++ translateExpr e1 (map (\(x, y) -> (x, y + 1)) pos)
+    ++ [Makeapp]
+translateExpr (Unary op e) pos =
+  translateExpr e pos
+    ++ [Pushpre (UnaryOperator op), Makeapp]
+translateExpr (Binary e1 op e2) pos =
+  translateExpr e2 pos
+    ++ translateExpr e1 (map (\(x, y) -> (x, y + 1)) pos)
+    ++ [Pushpre (BinaryOperator op), Makeapp, Makeapp]
+translateExpr (If e1 e2 e3) pos =
+  translateExpr e3 pos
+    ++ translateExpr e2 (map (\(x, y) -> (x, y + 1)) pos)
+    ++ translateExpr e1 (map (\(x, y) -> (x, y + 2)) pos)
+    ++ [Pushpre IfOperator, Makeapp, Makeapp, Makeapp]
+translateExpr (AtomicExpr (Number i)) pos = [Pushval (IntValue i)]
+translateExpr (AtomicExpr (TruthValue b)) pos = [Pushval (BoolValue b)]
+translateExpr (AtomicExpr (Var v)) pos =
+  case lookup v pos of
     Just i -> [Pushparam i]
     Nothing -> [Pushfun v]
 
-
+-- Add one definition to the initial machine state
 add1Definition :: MachineState -> Definition -> MachineState
-add1Definition ms@MachineState{code=c, heap=h, global=g} d@(Definition f args _) = ms{code=c ++ translateDef d, heap=h |> DEF (length c), global=Map.insert f (length h) g}
+add1Definition ms@MachineState {code = c, heap = h, global = g} d@(Definition f args _) =
+  ms
+    { code = c ++ translateDef d,
+      heap = h |> DEF (length c),
+      global = Map.insert f (length h) g
+    }
 
+-- Translate the program (multiple function definitions)
 translateProgram :: Program -> MachineState
-translateProgram = foldl add1Definition MachineState{pc=0, code=[
-    Reset, Pushfun "main", Call, Halt,
-    Pushparam 2, Unwind, Call, Pushparam 4, Unwind, Call, Operator2, UpdateOp, Return,
-    Pushparam 1, Unwind, Call, OperatorIf, Unwind, Call, UpdateOp, Return,
-    Pushparam 2, Unwind, Call, Operator1, UpdateOp, Return], stack=[], heap=Sequence.empty, global=Map.empty}
+translateProgram =
+  foldl
+    add1Definition
+    MachineState
+      { pc = 0,
+        code =
+          [ -- starting point
+            Reset,
+            Pushfun "main",
+            Call,
+            Halt,
+            -- binary operators
+            Pushparam 2,
+            Unwind,
+            Call,
+            Pushparam 4,
+            Unwind,
+            Call,
+            Operator2,
+            UpdateOp,
+            Return,
+            -- if-then-else
+            Pushparam 1,
+            Unwind,
+            Call,
+            OperatorIf,
+            Unwind,
+            Call,
+            UpdateOp,
+            Return,
+            -- unary operators
+            Pushparam 2,
+            Unwind,
+            Call,
+            Operator1,
+            UpdateOp,
+            Return
+          ],
+        -- the stack ist modelled as a list where the first element is the top of the stack
+        stack = [],
+        -- the heap is implemented with a finger tree
+        heap = Sequence.empty,
+        -- the global environment stores all the functions in a map (function name: heap address)
+        global = Map.empty
+      }
