37e2d54f8ad51b69d41f3369b45621ed5c2d4b25
Zhu Yang <zhuyang329@gmail.com>
2021-07-24 19:43:32 +0200

    formatted Lexer.hs

--- draft/Lexer.hs
+++ draft/Lexer.hs
@@ -1,24 +1,26 @@
-module Lexer (Token(..), tokenize) where
+module Lexer (Token (..), tokenize) where
 
-import Data.Char ( isAlpha, isDigit, isSpace )
-
+import Data.Char (isAlpha, isDigit, isSpace)
 
 data Token = NumberToken Integer | NameToken String | KeywordToken String deriving (Eq, Show)
 
 symbols = ['(', ')', '&', '|', '<', '+', '-', '*', '/', ';']
+
 keywords = ["let", "in", "if", "then", "else", "not", "true", "false"]
 
+-- Tokenize a string into a list of tokens
 tokenize :: String -> [Token]
 tokenize "" = []
-tokenize (c:s) 
-    | isSpace c = tokenize s
-    | isAlpha c = let token = c : takeWhile isAlpha s 
-                      rest =  dropWhile isAlpha s 
-                  in if token `elem` keywords
-                      then  KeywordToken token : tokenize rest
-                      else NameToken token : tokenize rest
-    | isDigit c = NumberToken (read(c : takeWhile isDigit s)) : tokenize (dropWhile isDigit s)
-    | c `elem` symbols = KeywordToken [c] : tokenize s
-    | c == '=' = case s of
-        '=':s' -> KeywordToken "==" : tokenize s'
-        _ -> KeywordToken "=" : tokenize s
+tokenize (c : s)
+  | isSpace c = tokenize s
+  | isAlpha c =
+    let token = c : takeWhile isAlpha s
+        rest = dropWhile isAlpha s
+     in if token `elem` keywords
+          then KeywordToken token : tokenize rest
+          else NameToken token : tokenize rest
+  | isDigit c = NumberToken (read (c : takeWhile isDigit s)) : tokenize (dropWhile isDigit s)
+  | c `elem` symbols = KeywordToken [c] : tokenize s
+  | c == '=' = case s of
+    '=' : s' -> KeywordToken "==" : tokenize s'
+    _ -> KeywordToken "=" : tokenize s
