7a36c41605b2d5b036ed5b86e86ec9bf3aa47c12
Zhu Yang <zhuyang329@gmail.com>
2021-08-07 10:53:17 +0200

    Run.hs now accepts multi-line input

--- src/Run.hs
+++ src/Run.hs
@@ -16,7 +16,17 @@ test s = case parseProgram (tokenize s) of
     Left message -> error $ "Runtime error: " ++ message
   Left message -> error $ "Syntax error: " ++ message
 
+-- Take in multi-line input until empty line
+multiline :: IO String
+multiline =
+  do
+    s <- getLine
+    case s of
+      "" -> return s
+      _ -> fmap (s ++) multiline
+
 main :: IO ()
-main = do
-  input <- getLine
-  putStrLn $ "Result: " ++ show (test input)
+main =
+  do
+    input <- multiline
+    putStrLn $ "==================\n" ++ "Result: " ++ show (test input)
